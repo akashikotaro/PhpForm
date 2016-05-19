686b86177ee8eeeb22c950588e4e9d264f944eb3
akashi <2014r01akashi@gmail.com>
2016-05-19 13:51:11 +0900

    PHP午前

---
+++ 01/form_test.php
@@ -0,0 +1,6 @@
+<form action="form_test2.php" method="GET">
+    <br />
+    <input type="text" name="name" placeholder="名前を入力してください">
+    <br /><br />
+    <input type="submit" value="送信">
+</form>
