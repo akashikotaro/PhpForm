14ddcab4d581ec53c726e7b3e41aff83ac86fe27
akashi <2014r01akashi@gmail.com>
2016-05-20 10:11:50 +0900

    php

--- 02/contact.php
+++ 02/contact.php
@@ -9,7 +9,7 @@
                 <tr>
                     <td class = menu>姓　[全角文字]<div class = require>必須</div></td>
                     <td>
-                        <input type="text" name="surname" size=12 required>
+                        <input type="text" name="surname" size=12 placeholder="例） 問合" required>
                     </td>
                 </tr>
 
@@ -17,7 +17,7 @@
                 <tr>
                     <td class = menu>名　[全角文字]<label class = require>必須</label></td>
                     <td>
-                        <input type="text" name="name" size=12 required>
+                        <input type="text" name="name" size=12 placeholder="例） 太郎"　required>
                     </td>
                 </tr>
 
@@ -35,7 +35,7 @@
                 <tr>
                     <td class = menu>住所<label class = require>必須</label></td>
                     <td>
-                        <input type="text" name="address" size=30 required>
+                        <input type="text" name="address" size=30 placeholder="例） 東京都 問合市 問合1-1"　required>
                     </td>
                 </tr>
 
@@ -43,9 +43,9 @@
                 <tr>
                     <td class = menu>電話番号<label class = require>必須</label></td>
                     <td>
-                        <input type="text" name="tel1" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" required> -
-                        <input type="text" name="tel2" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" required> -
-                        <input type="text" name="tel3" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" required>
+                        <input type="text" name="tel1" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" placeholder="例） 000" required> -
+                        <input type="text" name="tel2" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" placeholder="   0000" required> -
+                        <input type="text" name="tel3" size=4 maxlength="4" onKeyup="this.value=this.value.replace(/[^0-9]+/i,'')" placeholder="   0000"required>
                     </td>
                 </tr>
 
@@ -53,8 +53,8 @@
                 <tr>
                     <td class = menu>メールアドレス<label class = require>必須</label></td>
                     <td>
-                        <input type="text" name="email1" size=12 onKeyup="this.value=this.value.replace(/[^0-9a-z]+/i,'')" required> @
-                        <input type="text" name="email2" size=12 onKeyup="this.value=this.value.replace(/[^0-9a-z]+/i,'')" required>
+                        <input type="text" name="email1" size=12 onKeyup="this.value=this.value.replace(/[^0-9a-z]+/i,'')" placeholder="例） toiawase" required> @
+                        <input type="text" name="email2" size=12 onKeyup="this.value=this.value.replace(/[^0-9a-z]+/i,'')" placeholder=" toiawase.co.jp" required>
                     </td>
                 </tr>
 
@@ -85,7 +85,7 @@
                 <tr>
                     <td class = menu>お問い合わせ内容<label class = require>必須</label></td>
                     <td>
-                        <textarea name="content" rows=5 cols=55 required></textarea>
+                        <textarea name="content" rows=5 cols=55 placeholder="お問い合わせ内容を入力してください" required></textarea>
                     </td>
                 </tr>
 
